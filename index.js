716a84b7d3129b4a883735a55151d0eb7a9be6fb
deveshbis <deveshbiswas.cse@gmail.com>
2024-06-03 08:34:36 +0600

    all Useer Show post and get method to use

--- index.js
+++ index.js
@@ -16,68 +16,91 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
 
-    // articles collection
-    const articleCollection = client.db("newspaperDB").collection("articles") 
-
-
-    app.get("/articles", async(req, res)=> {
-        const result = await articleCollection.find().toArray()
-        res.send(result)
-    })
-
-    app.get('/singleDetails/:id', async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id) }
-        const result = await articleCollection.findOne(query);
-        res.send(result);
-    })
-
-    app.post('/articles', async (req, res) => {
-        const newsArticles = req.body;
-        console.log(newsArticles);
-        const result = await articleCollection.insertOne(newsArticles);
-        res.send(result);
-    })
+        // articles collection
+        const articleCollection = client.db("newspaperDB").collection("articles")
 
 
-    //Publisher Collection
-    const publisherCollection = client.db("newspaperDB").collection("publisher") 
+        app.get("/articles", async (req, res) => {
+            const result = await articleCollection.find().toArray()
+            res.send(result)
+        })
 
-    app.post('/publisher', async (req, res) => {
-        const publisherArticles = req.body;
-        console.log(publisherArticles);
-        const result = await publisherCollection.insertOne(publisherArticles);
-        res.send(result);
-    })
+        app.get('/singleDetails/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await articleCollection.findOne(query);
+            res.send(result);
+        })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.post('/articles', async (req, res) => {
+            const newsArticles = req.body;
+            console.log(newsArticles);
+            const result = await articleCollection.insertOne(newsArticles);
+            res.send(result);
+        })
+
+
+        //users Collection
+        const userCollection = client.db("newspaperDB").collection("users")
+
+        app.get("/users", async (req, res) => {
+            const result = await userCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query)
+            if (existingUser) {
+                return res.send({ message: "user already exits", insertedId: null })
+            }
+            const result = await userCollection.insertOne(user)
+            res.send(result)
+        })
+
+
+
+
+
+        //Publisher Collection
+        const publisherCollection = client.db("newspaperDB").collection("publisher")
+
+        app.post('/publisher', async (req, res) => {
+            const publisherArticles = req.body;
+            console.log(publisherArticles);
+            const result = await publisherCollection.insertOne(publisherArticles);
+            res.send(result);
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("Newspaper Server is running")
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`newspaper Server is running ${port}`);
 })

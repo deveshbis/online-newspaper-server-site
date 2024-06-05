d090fbd2d3e7a0c9d0a91496c8cbbe046db5f2e5
deveshbis <deveshbiswas.cse@gmail.com>
2024-06-05 14:57:52 +0600

    updated

--- index.js
+++ index.js
@@ -29,26 +29,30 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
 
-        //jwt 
-        app.post("/jwt", async (req, res) => {
+
+        // articles collection
+        const articleCollection = client.db("newspaperDB").collection("articles")
+        const userCollection = client.db("newspaperDB").collection("users")
+
+
+
+        // jwt related api
+        app.post('/jwt', async (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRTE, {
-                expiresIn: "365d"
-            })
-            res.send({ token })
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token });
         })
 
-
-        //middleware
-        const verfifyToken = (req, res, next) => {
-            console.log("inside verify token", req.headers.authorization);
+        // middlewares 
+        const verifyToken = (req, res, next) => {
+            console.log('inside verify token', req.headers.authorization);
             if (!req.headers.authorization) {
-                return res.status(401).send({ message: 'unauthorized access' })
+                return res.status(401).send({ message: 'unauthorized access' });
             }
             const token = req.headers.authorization.split(' ')[1];
-            jwt.verify(token, process.env.ACCESS_TOKEN_SECRTE, (err, decoded) => {
+            jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
                 if (err) {
-                    return res.status(401).send({ message: "forbidden access" })
+                    return res.status(401).send({ message: 'unauthorized access' })
                 }
                 req.decoded = decoded;
                 next();
@@ -56,13 +60,75 @@ async function run() {
         }
 
 
+        // use verify admin after verifyToken
+        const verifyAdmin = async (req, res, next) => {
+            const email = req.decoded.email;
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            const isAdmin = user?.role === 'admin';
+            if (!isAdmin) {
+                return res.status(403).send({ message: 'forbidden access' });
+            }
+            next();
+        }
+
+        //user related api
+
+        app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        });
+
+
+        app.get('/users/admin/:email', verifyToken, async (req, res) => {
+            const email = req.params.email;
+
+            if (email !== req.decoded.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+
+            const query = { email: email };
+            const user = await userCollection.findOne(query);
+            let admin = false;
+            if (user) {
+                admin = user?.role === 'admin';
+            }
+            res.send({ admin });
+        })
 
 
 
-        // articles collection
-        const articleCollection = client.db("newspaperDB").collection("articles")
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            // insert email if user doesnt exists: 
+            // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
+            const query = { email: user.email }
+            const existingUser = await userCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
 
 
+        app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+        })
+
+
+
+
+
+        // articles related api
         app.get("/articles", async (req, res) => {
             const result = await articleCollection.find().toArray()
             res.send(result)
@@ -83,70 +149,7 @@ async function run() {
         })
 
 
-        //users Collection
-        const userCollection = client.db("newspaperDB").collection("users")
 
-        //use verify admin
-        const verifyAdmin = async (req, res, next) => {
-            const email = req.decoded.email;
-            const query = { email: email }
-            const user = await userCollection.findOne(query)
-            const isAdmin = user?.role === 'admin';
-            if (!isAdmin) {
-                return res.status(403).send({ message: 'forbidden access' })
-            }
-            next()
-        }
-
-        app.get("/users", verfifyToken, verifyAdmin, async (req, res) => {
-            const result = await userCollection.find().toArray()
-            res.send(result)
-        })
-
-
-
-        app.post("/users", async (req, res) => {
-            const user = req.body;
-            const query = { email: user.email }
-            const existingUser = await userCollection.findOne(query)
-            if (existingUser) {
-                return res.send({ message: "user already exits", insertedId: null })
-            }
-            const result = await userCollection.insertOne(user)
-            res.send(result)
-        })
-
-
-
-        app.patch("/users/admin/:id", verfifyToken, verifyAdmin, async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) }
-            const updatedDoc = {
-                $set: {
-                    role: 'admin'
-                }
-            }
-            const result = await userCollection.updateOne(filter, updatedDoc)
-            res.send(result)
-        })
-
-
-
-
-        //Admin
-        app.get("/users/admin/:email", verfifyToken, async (req, res) => {
-            const email = req.params.email;
-            if (email !== req.decoded.email) {
-                return res.status(403).send({ message: "unauthorized access" })
-            }
-            const query = { email: email }
-            const user = await userCollection.findOne(query)
-            let admin = false;
-            if (user) {
-                admin = user?.role === 'admin';
-            }
-            res.send({ admin })
-        })
 
 
 
@@ -155,12 +158,12 @@ async function run() {
         //Admin Publisher
         const adminPublisherCollection = client.db("newspaperDB").collection("adminPublisher")
 
-        app.get("/adminPublisher", verfifyToken, verifyAdmin, async (req, res) => {
+        app.get("/adminPublisher", verifyToken,  async (req, res) => {
             const result = await adminPublisherCollection.find().toArray()
             res.send(result)
         })
 
-        app.post('/adminPublisher', verfifyToken, verifyAdmin, async (req, res) => {
+        app.post('/adminPublisher', verifyToken, verifyAdmin, async (req, res) => {
             const adminPublisherArticles = req.body;
             console.log(adminPublisherArticles);
             const result = await adminPublisherCollection.insertOne(adminPublisherArticles);
@@ -168,21 +171,6 @@ async function run() {
         })
 
 
-
-
-
-
-
-
-        //Publisher Collection
-        const publisherCollection = client.db("newspaperDB").collection("publisher")
-
-        app.post('/publisher', async (req, res) => {
-            const publisherArticles = req.body;
-            console.log(publisherArticles);
-            const result = await publisherCollection.insertOne(publisherArticles);
-            res.send(result);
-        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
